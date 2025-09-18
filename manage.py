6d327a9bb27f6405b89aadf28f9f6ead46772b5c
yagnik5775 <yagniknarola1234@gmail.com>
2025-09-18 14:21:59 +0530

    changes

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 
+print("hello world")
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'document_intelligence.settings')

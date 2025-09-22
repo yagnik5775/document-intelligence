953209eaaa7f4a26d587b814b662dd38e2adcee9
yagnik5775 <yagniknarola1234@gmail.com>
2025-09-22 15:39:51 +0530

    views.py

--- document_processing/views.py
+++ document_processing/views.py
@@ -34,17 +34,11 @@ logger = logging.getLogger(__name__)
 
 # Load the FAISS index and metadata/
 
-vector_folder = r"Y:\YBAI\Document_AI\document_intelligence\document_processing\vector_store"
-faiss_index = faiss.read_index(f"{vector_folder}/faiss_index.faiss")
 
+vector_folder = "/app/document_processing/vector_store"
+faiss_index = faiss.read_index(f"{vector_folder}/faiss_index.faiss")
 with open(f"{vector_folder}/chunk_metadata.json", "r", encoding="utf-8") as f:
     chunk_metadata = json.load(f)
-
-
-# vector_folder = "/app/document_processing/vector_store"
-# faiss_index = faiss.read_index(f"{vector_folder}/faiss_index.faiss")
-# with open(f"{vector_folder}/chunk_metadata.json", "r", encoding="utf-8") as f:
-#     chunk_metadata = json.load(f)
 
 # Load the SentenceTransformer model
 model = SentenceTransformer("all-MiniLM-L6-v2")
